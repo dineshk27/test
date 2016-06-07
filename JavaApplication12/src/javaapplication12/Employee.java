1346aaa28954b85870b00bd3504bf35114142b87
dineshk <dineshk@suntecgroup.com>
2016-06-07 12:20:54 +0530

    Employee Class added

---
+++ JavaApplication12/src/javaapplication12/Employee.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication12;
+
+/**
+ *
+ * @author dineshk
+ */
+public class Employee {
+    
+    private int empId;
+    private String empName;
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+    
+    
+}
